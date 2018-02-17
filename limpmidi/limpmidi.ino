4688d2ad7e29b8434e3385db07ee525ede0c1ecf
Misael.K <misael_k@yahoo.com.ar>
2018-02-16 22:10:34 -0300

    Added serial IO. Less magic numbers.

--- limpmidi/limpmidi.ino
+++ limpmidi/limpmidi.ino
@@ -1,10 +1,34 @@
+const bool USE_SERIAL = true;
+const long int SERIAL_BPS = 115200;
+
 const int PIN_LED = 13;
 const int PIN_CONTROL_STEP = 10; // rojo
 const int PIN_CONTROL_DIRECTION = 9; // azul
-const int MAX_POSITION = 80;
+const int MAX_POSITION = 2; // fd has 80 tracks
 
-const int PERIOD_START = 35; // must be larger than 0
-const int PERIOD_END = 42;
+const unsigned int PERIOD_START = 1; // must be larger than 0
+const unsigned int PERIOD_END = 1000;
+
+// 440Hz
+int A  = 42; // 1
+int Ab = 00; // 2
+int G  = 00; // 3
+int Gb = 00; // 4
+int F  = 00; // 5
+int E  = 57; // 6
+int Eb = 00; // 7
+int D  = 00; // 8
+int Db = 00; // 9
+int C  = 00; // 10
+int B  = 00; // 11
+int A  = 84; // 12
+// 220Hz
+
+//const PERIOD_LIST[] = 
+
+const int CYCLE_PERIOD = 22; // microseconds
+const unsigned long NOTE_CHANGE_THRESHOLD_MS = 200; // ms
+const unsigned long NOTE_CHANGE_THRESHOLD = NOTE_CHANGE_THRESHOLD_MS * 1000 / CYCLE_PERIOD; //300ms
 
 
 const int DIRECTION_BACKWARDS = HIGH;
@@ -13,10 +37,26 @@ const int DIRECTION_FORWARDS = LOW;
 unsigned long interruptCounter = 0;
 unsigned long currentPosition = 0;
 unsigned long tick = 0;
-unsigned long currentPeriod = 0;
 unsigned long noteChangeCounter = 0;
+unsigned int currentPeriod = 0;
+
+byte incomingByte = 0;
+unsigned int incomingInt = 0;
+
+char debug_string_buffer[20];
+// sprintf + serial of 20 bytes takes ~200us
+// sprintf + serial of 10 bytes takes ~144us
+// sprintf + serial of  5 bytes takes ~108us
+#define debug(formato, valor) \
+  sprintf(debug_string_buffer, formato, valor); \
+  Serial.print(debug_string_buffer)
 
 void setup() {
+  if (USE_SERIAL) {
+    Serial.begin(SERIAL_BPS);
+    Serial.setTimeout(10); // parseInt uses this timeout
+  }
+  
   randomSeed(analogRead(0)); // init random seed
   
   pinMode(PIN_LED, OUTPUT);
@@ -31,11 +71,18 @@ void setup() {
   // 5ms * (158 / 2) = 395ms
   digitalWrite(PIN_CONTROL_DIRECTION, DIRECTION_BACKWARDS);
   for (int i = 0; i < MAX_POSITION; i++) {
-    digitalWrite(PIN_CONTROL_STEP, HIGH);
     digitalWrite(PIN_CONTROL_STEP, LOW);
+    digitalWrite(PIN_CONTROL_STEP, HIGH);
+    delay(10);
+  }
+  digitalWrite(PIN_CONTROL_DIRECTION, DIRECTION_FORWARDS);
+  for (int i = 0; i < MAX_POSITION / 2; i++) {
+    digitalWrite(PIN_CONTROL_STEP, LOW);
+    digitalWrite(PIN_CONTROL_STEP, HIGH);
     delay(10);
   }
   digitalWrite(PIN_LED, LOW);
+
   delay(1000);
   
   currentPeriod = PERIOD_START; // possible start note
@@ -46,7 +93,7 @@ void loop() {
   
   /*interruptCounter++;
   // 40us * 10000 = 400ms
-  if (interruptCounter == 10000) {
+  if (interruptCounter == 10) {
     digitalWrite(PIN_LED, (digitalRead(PIN_LED) == LOW) ? HIGH : LOW);
     interruptCounter = 0;
   }*/
@@ -56,6 +103,7 @@ void loop() {
   if (tick == currentPeriod) {
     digitalWrite(PIN_CONTROL_STEP, HIGH);
     digitalWrite(PIN_CONTROL_STEP, LOW);
+    //digitalWrite(PIN_LED, (digitalRead(PIN_LED) == LOW) ? HIGH : LOW);
     currentPosition++;
     tick = 0;
   }
@@ -65,17 +113,37 @@ void loop() {
     currentPosition = 0;
   }
   
-  
-  noteChangeCounter++;
-  if (noteChangeCounter == 30000) {
+  // every 40ms, change note
+  /*noteChangeCounter++;
+  if (noteChangeCounter == NOTE_CHANGE_THRESHOLD) {
     digitalWrite(PIN_LED, (digitalRead(PIN_LED) == LOW) ? HIGH : LOW);
     noteChangeCounter = 0;
-    currentPeriod++;
-    if (currentPeriod == PERIOD_END) { // possible end note
-      currentPeriod = PERIOD_START;
+    //currentPeriod = currentPeriod + 1;
+    //if (currentPeriod > PERIOD_END) { // possible end note
+    //  currentPeriod = PERIOD_START;
+    //}
+    currentPeriod = random(PERIOD_START, PERIOD_END);
+    tick = 0;
+    if (USE_SERIAL) {
+      debug("%i ", currentPeriod);
+      Serial.print("\n");
+    }
+  }*/
+
+  if (USE_SERIAL) {
+    if (Serial.available() > 0) {
+      //incomingByte = Serial.read();
+      //if (incomingByte == 'n') {
+        incomingInt = Serial.parseInt();
+        if (incomingInt >= PERIOD_START && incomingInt <= PERIOD_END) {
+          Serial.println(incomingInt);
+          currentPeriod = incomingInt;
+          tick = 0;
+        }
+      //}
     }
   }
   
   //delay(10);
-  delayMicroseconds(40);
+  delayMicroseconds(CYCLE_PERIOD);
 }
