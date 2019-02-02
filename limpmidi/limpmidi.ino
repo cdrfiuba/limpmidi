09069d51c38d5eb8aa14d0afc150a560cdb9e0b1
Misael.K <misael_k@yahoo.com.ar>
2019-02-01 21:28:02 -0300

    Testing some stuff. Unstable.

--- limpmidi/limpmidi.ino
+++ limpmidi/limpmidi.ino
@@ -1,5 +1,6 @@
 #include <TimerOne.h>
 #include <MIDI.h>
+#include <MIDIUSB.h>
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
@@ -117,11 +118,27 @@ char debug_string_buffer[20];
   sprintf(debug_string_buffer, formato, valor); \
   Serial.print(debug_string_buffer)
 
+/*
+
+http://web.archive.org/web/20180206075622/http://www.electronics.dit.ie/staff/tscarff/Music_technology/midi/midi_note_numbers_for_octaves.htm
+
+                        Note Numbers                  
+Octave   C  C#   D  D#   E   F  F#   G  G#   A  A#   B 
+  0    000 001 002 003 004 005 006 007 008 009 010 011
+  1    012 013 014 015 016 017 018 019 020 021 022 023
+  2    024 025 026 027 028 029 030 031 032 033 034 035
+  3    036 037 038 039 040 041 042 043 044 045 046 047
+  4    048 049 050 051 052 053 054 055 056 057 058 059
+  5    060 061 062 063 064 065 066 067 068 069 070 071
+  6    072 073 074 075 076 077 078 079 080 081 082 083
+  7    084 085 086 087 088 089 090 091 092 093 094 095
+  8    096 097 098 099 100 101 102 103 104 105 106 107
+  9    108 109 110 111 112 113 114 115 116 117 118 119
+ 10    120 121 122 123 124 125 126 127 
+ 
+*/
+
 void setup() {
-  if (USE_SERIAL) {
-    Serial.begin(SERIAL_BPS);
-    Serial.setTimeout(10); // parseInt uses this timeout
-  }
   
   //randomSeed(analogRead(0)); // init random seed
   
@@ -177,6 +194,10 @@ void setup() {
   MIDI.setHandleNoteOff(handleMIDINoteOff);
   MIDI.begin(MIDI_CHANNEL_OMNI);
   
+  if (USE_SERIAL) {
+    Serial.begin(SERIAL_BPS);
+    Serial.setTimeout(10); // parseInt uses this timeout
+  }
 }
 
 void noteSetter() {
@@ -205,18 +226,27 @@ void noteSetter() {
 
 void handleMIDINoteOn(byte inChannel, byte inNote, byte inVelocity) {
   inChannel = inChannel; inVelocity = inVelocity;
-  Serial.print("On ");
-  Serial.println(inNote);
+  MIDI.sendNoteOn(inNote, inVelocity, inChannel);
+  //Serial.print("On ");
+  //Serial.println(inNote);
 }
 void handleMIDINoteOff(byte inChannel, byte inNote, byte inVelocity) {
   inChannel = inChannel; inVelocity = inVelocity;
-  Serial.print("Off ");
-  Serial.println(inNote);
+  MIDI.sendNoteOff(inNote, inVelocity, inChannel);
+  //Serial.print("Off ");
+  //Serial.println(inNote);
 }
 
 void loop() {
   
   MIDI.read();
+  
+  delay(1000);
+  MIDI.sendNoteOn(60, 127, 1);
+  delay(100);
+  MIDI.sendNoteOff(60, 127, 1);
+  digitalWrite(PIN_LED, abs(digitalRead(PIN_LED) - 1));
+  
   
   // every noteChangeThreshold ms, change note
   if (autoPlay) {
